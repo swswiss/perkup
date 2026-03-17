9d5de586ccdfaa08dc9146645324e1fbe57e717a
swswiss <stefanachesilviu1993@gmail.com>
2026-03-17 14:00:35 +0200

    saddd

--- app/service/onesignal_service.rb
+++ app/service/onesignal_service.rb
@@ -1,27 +1,26 @@
 class OnesignalService
-  BASE_URL = "https://api.onesignal.com/notifications".freeze
-                                                                                                                   
-  def self.send_to_user(user, title:, message:, url: nil)                                                          
-    return unless ONESIGNAL_CONFIG[:app_id] && ONESIGNAL_CONFIG[:api_key]                                          
-                                                                                                                   
-    body = {    
-      app_id: ONESIGNAL_CONFIG[:app_id],                                                                           
-      include_aliases: { external_id: [user.id.to_s] },
-      target_channel: "push",                                                                                      
-      headings: { en: title },                                                                                     
-      contents: { en: message }                                                                                    
-    }                                                                                                              
+  BASE_URL = "https://onesignal.com/api/v1/notifications".freeze
+
+  def self.send_to_user(user, title:, message:, url: nil)
+    return unless ONESIGNAL_CONFIG[:app_id] && ONESIGNAL_CONFIG[:api_key]
+
+    body = {
+      app_id: ONESIGNAL_CONFIG[:app_id],
+      include_external_user_ids: [user.id.to_s],
+      headings: { en: title },
+      contents: { en: message }
+    }
     body[:url] = url if url
 
-    HTTParty.post(                                                                                                 
+    HTTParty.post(
       BASE_URL,
-      headers: {                                                                                                   
-        "Authorization" => "Key #{ONESIGNAL_CONFIG[:api_key]}",
+      headers: {
+        "Authorization" => "Basic #{ONESIGNAL_CONFIG[:api_key]}",
         "Content-Type" => "application/json"
-      },                                                                                                           
+      },
       body: body.to_json
-    )                                                                                                              
+    )
   rescue StandardError => e
     Rails.logger.error("OneSignal notification failed: #{e.message}")
-  end                                                                                                              
+  end
 end
