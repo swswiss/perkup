e3b93d160b2987699edfe7aff68b073642464f32
swswiss <stefanachesilviu1993@gmail.com>
2026-03-17 14:06:27 +0200

    saddds

--- config/initializers/onesignal.rb
+++ config/initializers/onesignal.rb
@@ -1,4 +1,4 @@
 ONESIGNAL_CONFIG = {
-  app_id: "608efa7a-fb7a-476e-8376-b2969d7810b9",                                                  
-  api_key: "os_v2_app_mchpu6x3pjdw5a3wwklj26aqxhtcx27lwscesnm7726k6tsm3icnlfrhrbqzth2cb6rdqxgb4plba7xtr6u6lkw7o5nuo5yfffitdly"
+  app_id: "608efa7a-fb7a-476e-8376-b2969d7810b9",                                          
+  api_key: "os_v2_app_mchpu6x3pjdw5a3wwklj26aqxerf6tpnel4eqn57muozoqjmqe2tirritkkfvuok23fvwzdec2cd7twdhzfgplgudgvv6ypimeuhy6a"
 }.freeze
