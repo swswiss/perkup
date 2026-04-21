6323d8fed9b58c39199dc10ad584faf5372e6003
swswiss <stefanachesilviu1993@gmail.com>
2026-04-21 14:56:15 +0300

    typo

--- config/initializers/web_push.rb
+++ config/initializers/web_push.rb
@@ -1,11 +1,14 @@
 require "yaml"
 
 module Perkup
-  VAPID = begin
+  VAPID = if Rails.env.production?
+    {
+      public_key: ENV["VAPID_PUBLIC_KEY"],
+      private_key: ENV["VAPID_PRIVATE_KEY"]
+    }
+  else
     path = Rails.root.join("config", "vapid.yml")
     raw = YAML.load_file(path, aliases: true)
     raw[Rails.env] || raw["default"] || {}
-  rescue Errno::ENOENT
-    {}
   end.with_indifferent_access.freeze
 end
